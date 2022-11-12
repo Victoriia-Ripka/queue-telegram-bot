2cde21f8b9d2ec813f5e71710debf7312543ac69
Victoriia-Ripka <vikaripka@gmail.com>
2022-11-12 17:59:56 +0100

    echo_bot

--- bot.py
+++ bot.py
@@ -1,5 +1,19 @@
 import telebot
+
 # треба ще лібу цю скачати
+# pip install pyTelegramBotAPI
 
 bot = telebot.TeleBot('5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k')
 
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
