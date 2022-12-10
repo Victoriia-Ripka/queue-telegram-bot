2aeebad80987d43c5c1bcfcbab8607987e0bbf3b
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-10 14:26:17 +0100

    edits

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-# Объект бота
 bot = Bot(token="5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k")
-# Диспетчер для бота
 dp = Dispatcher(bot)
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 
 
-# Хэндлер на команду /test1
 @dp.message_handler(commands="test1")
 async def cmd_test1(message: types.Message):
     await message.reply("Test 1")
