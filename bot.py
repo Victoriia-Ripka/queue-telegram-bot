328ecea59b3611100be58f4319dea7866a9cee25
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2022-12-14 14:12:25 +0100

    тестовий коміт 2

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
-print("test")
-
 bot = Bot(token="5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
