2060e2d48a93a6c4d2a6776da1a108d970a2daaf
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2022-12-14 14:03:11 +0100

    тестовий коміт

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 
+print("test")
+
 bot = Bot(token="5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k")
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
@@ -11,3 +13,5 @@ async def cmd_test1(message: types.Message):
     await message.reply("Test 1")
 
 executor.start_polling(dp, skip_updates=True)
+
+
