fb75a3e0318a36334f25fbc40f7ee3f079431833
Victoriia-Ripka <vikaripka@gmail.com>
2022-12-18 13:00:59 +0100

    connect to db

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
+from datetime import datetime
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import db
 
-print("bebra")
+API_TOKEN = '5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k'
 
-bot = Bot(token="5626939602:AAHRuLoS6EaWY1OfVHdIn0tBYeLzC6ZZY1k")
-dp = Dispatcher(bot)
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 logging.basicConfig(level=logging.INFO)
 
+my_cursor = db.mydb.cursor()
 
-@dp.message_handler(commands="test1")
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+class Form(StatesGroup):
+    pass
 
-executor.start_polling(dp, skip_updates=True)
+@dp.message_handler(commands="start")
+async def start(message: types.Message):
+    # при старті потрібно запускати свтворення дб, а при завершені - все видаляти. 
+    # хіба це потрібно зробити в мейн
+    await message.answer("hello, user")
 
+if __name__ == '__main__':
+    try:
+        executor.start_polling(dp, skip_updates=True)
+    except Exception as error:
+        print('Cause: {}'.format(error))
