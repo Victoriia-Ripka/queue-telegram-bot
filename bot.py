82174474bc2fac12a8b256487a64f907ffcaae71
Vitalii <solonychnui.vitaliy@lll.kpi.ua>
2023-01-03 16:19:59 +0100

    Закінчено функцію delete_queue. Необхідно відтестувати усі функції та можна мержити в мейн

--- bot.py
+++ bot.py
@@ -21,15 +21,111 @@ class Form(StatesGroup):
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    # при старті потрібно запускати свтворення дб, а при завершені - все видаляти.
-    # хіба це потрібно зробити в мейн
+    chat_name = "user"
+    # chat_name = message["chat"]["first_name"]
+    # how to get chat_name in chat and use it in answer?
+    await message.answer("hello, ", chat_name)
 
-    # Так цейво, чому ми маємо створювати/видаляти. Ми маємо підкючати існуючу базу даних
-    # І передбачити можливість якогось очищення непотрібних записів
-    await message.answer("hello, user")
+@dp.message_handler(commands="help")
+async def help(message: types.Message):
+    text = "all commands definitions will be here soon"
+    await message.answer(text)
+
+@dp.message_handler(commands="end")
+async def help(message: types.Message):
+    print("Start deletind DB...")
+    sql_command = """DROP TABLE IF EXISTS `queue-bot-kpi`.`Teachers` ;"""
+    my_cursor.execute(sql_command)
+    sql_command = """DROP TABLE IF EXISTS `queue-bot-kpi`.`Sign_ups` ;"""
+    my_cursor.execute(sql_command)
+    sql_command = """DROP TABLE IF EXISTS `queue-bot-kpi`.`Queues` ;"""
+    my_cursor.execute(sql_command)
+    sql_command = """DROP TABLE IF EXISTS `queue-bot-kpi`.`Students` ;"""
+    my_cursor.execute(sql_command)
+    sql_command = """DROP TABLE IF EXISTS `queue-bot-kpi`.`Subjects` ;"""
+    my_cursor.execute(sql_command)
+    print("All tables are deleted")
+    text = "All tables are deleted"
+    await message.answer(text)
+
 
 if __name__ == '__main__':
     try:
+        print("Initializing Database...")
+        print("Connected to the database")
+
+        # Command that creates the "oders" table
+        sql_command = """CREATE DATABASE IF NOT EXISTS `queue-bot-kpi` DEFAULT CHARACTER SET utf8 ;"""
+        my_cursor.execute(sql_command)
+        sql_command = """CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Subjects` (
+  `subject_id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `id_teacher` INT NOT NULL,
+  PRIMARY KEY (`subject_id`),
+  UNIQUE INDEX `title_UNIQUE` (`title` ASC) VISIBLE,
+  UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE);"""
+        my_cursor.execute(sql_command)
+        sql_command = """CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Students` (
+  `telegram_user_id` INT NOT NULL,
+  `username` VARCHAR(45) NULL,
+  `firstname` VARCHAR(45) NULL,
+  PRIMARY KEY (`telegram_user_id`),
+  UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);"""
+        my_cursor.execute(sql_command)
+        sql_command = """CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Queues` (
+  `id_queue` INT NOT NULL AUTO_INCREMENT,
+  `subject_id` INT NOT NULL,
+  PRIMARY KEY (`id_queue`),
+  UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
+  UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
+  CONSTRAINT `subject_id fk from Queue to Subjects`
+    FOREIGN KEY (`subject_id`)
+    REFERENCES `queue-bot-kpi`.`Subjects` (`subject_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE);"""
+        my_cursor.execute(sql_command)
+        sql_command = """CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Sign_ups` (
+  `id_sign_up` INT NOT NULL AUTO_INCREMENT,
+  `id_queue` INT NOT NULL,
+  `telegram_user_id` INT NOT NULL,
+  `position` INT NOT NULL,
+  PRIMARY KEY (`id_sign_up`),
+  UNIQUE INDEX `position_UNIQUE` (`position` ASC) VISIBLE,
+  UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
+  UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
+  CONSTRAINT `id_queue fk from Sign_ups to Queue`
+    FOREIGN KEY (`id_queue`)
+    REFERENCES `queue-bot-kpi`.`Queues` (`id_queue`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `telegram_user_id fk from Sign_ups to Students`
+    FOREIGN KEY (`telegram_user_id`)
+    REFERENCES `queue-bot-kpi`.`Students` (`telegram_user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);"""
+        my_cursor.execute(sql_command)
+        sql_command = """CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Teachers` (
+  `id_teacher` INT NOT NULL AUTO_INCREMENT,
+  `username_telegram` VARCHAR(45) NULL,
+  `phone_number` CHAR(13) NULL,
+  `email` VARCHAR(60) NULL,
+  `info` TEXT(1000) NULL,
+  PRIMARY KEY (`id_teacher`),
+  UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE,
+  UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  CONSTRAINT `id_teacher fk from Teachers to Subjects`
+    FOREIGN KEY (`id_teacher`)
+    REFERENCES `queue-bot-kpi`.`Subjects` (`id_teacher`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);"""
+        my_cursor.execute(sql_command)
+        print("All tables are ready")
+        print("Bot Started")
         executor.start_polling(dp, skip_updates=True)
+
     except Exception as error:
         print('Cause: {}'.format(error))
+
--- sql_file.sql
+++ sql_file.sql
@@ -4,124 +4,114 @@
 -- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
-SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
-        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema queue-bot
+-- Schema mydb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema queue-bot
+-- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `queue-bot` DEFAULT CHARACTER SET utf8;
-USE `queue-bot`;
+CREATE SCHEMA IF NOT EXISTS `queue-bot-kpi` DEFAULT CHARACTER SET utf8 ;
+USE `queue-bot-kpi` ;
 
 -- -----------------------------------------------------
--- Table `queue-bot`.`Subjects`
+-- Table `queue-bot-kpi`.`Subjects`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `queue-bot`.`Subjects`;
+DROP TABLE IF EXISTS `queue-bot-kpi`.`Subjects` ;
 
-CREATE TABLE IF NOT EXISTS `queue-bot`.`Subjects`
-(
-    `subject_id` INT          NOT NULL AUTO_INCREMENT,
-    `title`      VARCHAR(100) NOT NULL,
-    `id_teacher` INT          NOT NULL,
-    PRIMARY KEY (`subject_id`),
-    UNIQUE INDEX `title_UNIQUE` (`title` ASC) VISIBLE,
-    UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
-    UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE
-)
-    ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Subjects` (
+  `subject_id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `id_teacher` INT NOT NULL,
+  PRIMARY KEY (`subject_id`),
+  UNIQUE INDEX `title_UNIQUE` (`title` ASC) VISIBLE,
+  UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
+  UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `queue-bot`.`Students`
+-- Table `queue-bot-kpi`.`Students`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `queue-bot`.`Students`;
+DROP TABLE IF EXISTS `queue-bot-kpi`.`Students` ;
 
-CREATE TABLE IF NOT EXISTS `queue-bot`.`Students`
-(
-    `telegram_user_id` INT         NOT NULL,
-    `username`         VARCHAR(45) NULL,
-    `firstname`        VARCHAR(45) NULL,
-    PRIMARY KEY (`telegram_user_id`),
-    UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
-    UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE
-)
-    ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Students` (
+  `telegram_user_id` INT NOT NULL,
+  `username` VARCHAR(45) NULL,
+  `firstname` VARCHAR(45) NULL,
+  PRIMARY KEY (`telegram_user_id`),
+  UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `queue-bot`.`Queues`
+-- Table `queue-bot-kpi`.`Queues`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `queue-bot`.`Queues`;
+DROP TABLE IF EXISTS `queue-bot-kpi`.`Queues` ;
 
-CREATE TABLE IF NOT EXISTS `queue-bot`.`Queues`
-(
-    `id_queue`   INT NOT NULL AUTO_INCREMENT,
-    `subject_id` INT NOT NULL,
-    PRIMARY KEY (`id_queue`),
-    UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
-    UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
-    CONSTRAINT `subject_id fk from Queue to Subjects`
-        FOREIGN KEY (`subject_id`)
-            REFERENCES `queue-bot`.`Subjects` (`subject_id`)
-            ON DELETE CASCADE
-            ON UPDATE CASCADE
-)
-    ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Queues` (
+  `id_queue` INT NOT NULL AUTO_INCREMENT,
+  `subject_id` INT NOT NULL,
+  PRIMARY KEY (`id_queue`),
+  UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
+  UNIQUE INDEX `subject_id_UNIQUE` (`subject_id` ASC) VISIBLE,
+  CONSTRAINT `subject_id fk from Queue to Subjects`
+    FOREIGN KEY (`subject_id`)
+    REFERENCES `queue-bot-kpi`.`Subjects` (`subject_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `queue-bot`.`Sign_ups`
+-- Table `queue-bot-kpi`.`Sign_ups`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `queue-bot`.`Sign_ups`;
+DROP TABLE IF EXISTS `queue-bot-kpi`.`Sign_ups` ;
 
-CREATE TABLE IF NOT EXISTS `queue-bot`.`Sign_ups`
-(
-    `id_sign_up`       INT NOT NULL AUTO_INCREMENT,
-    `id_queue`         INT NOT NULL,
-    `telegram_user_id` INT NOT NULL,
-    `position`         INT NOT NULL,
-    PRIMARY KEY (`id_sign_up`),
-    UNIQUE INDEX `position_UNIQUE` (`position` ASC) VISIBLE,
-    UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
-    UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
-    CONSTRAINT `id_queue fk from Sign_ups to Queue`
-        FOREIGN KEY (`id_queue`)
-            REFERENCES `queue-bot`.`Queues` (`id_queue`)
+CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Sign_ups` (
+  `id_sign_up` INT NOT NULL AUTO_INCREMENT,
+  `id_queue` INT NOT NULL,
+  `telegram_user_id` INT NOT NULL,
+  `position` INT NOT NULL,
+  PRIMARY KEY (`id_sign_up`),
+  UNIQUE INDEX `position_UNIQUE` (`position` ASC) VISIBLE,
+  UNIQUE INDEX `id_queue_UNIQUE` (`id_queue` ASC) VISIBLE,
+  UNIQUE INDEX `telegram_user_id_UNIQUE` (`telegram_user_id` ASC) VISIBLE,
+  CONSTRAINT `id_queue fk from Sign_ups to Queue`
+    FOREIGN KEY (`id_queue`)
+    REFERENCES `queue-bot-kpi`.`Queues` (`id_queue`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `telegram_user_id fk from Sign_ups to Students`
-        FOREIGN KEY (`telegram_user_id`)
-            REFERENCES `queue-bot`.`Students` (`telegram_user_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
+    FOREIGN KEY (`telegram_user_id`)
+    REFERENCES `queue-bot-kpi`.`Students` (`telegram_user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `queue-bot`.`Teachers`
+-- Table `queue-bot-kpi`.`Teachers`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `queue-bot`.`Teachers`;
+DROP TABLE IF EXISTS `queue-bot-kpi`.`Teachers` ;
 
-CREATE TABLE IF NOT EXISTS `queue-bot`.`Teachers`
-(
-    `id_teacher`        INT         NOT NULL AUTO_INCREMENT,
-    `username_telegram` VARCHAR(45) NULL,
-    `phone_number`      CHAR(13)    NULL,
-    `email`             VARCHAR(60) NULL,
-    `info`              TEXT(1000)  NULL,
-    PRIMARY KEY (`id_teacher`),
-    UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE,
-    UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE,
-    UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
-    CONSTRAINT `id_teacher fk from Teachers to Subjects`
+CREATE TABLE IF NOT EXISTS `queue-bot-kpi`.`Teachers` (
+  `id_teacher` INT NOT NULL AUTO_INCREMENT,
+  `username_telegram` VARCHAR(45) NULL,
+  `phone_number` CHAR(13) NULL,
+  `email` VARCHAR(60) NULL,
+  `info` TEXT(1000) NULL,
+  PRIMARY KEY (`id_teacher`),
+  UNIQUE INDEX `id_teacher_UNIQUE` (`id_teacher` ASC) VISIBLE,
+  UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  CONSTRAINT `id_teacher fk from Teachers to Subjects`
     FOREIGN KEY (`id_teacher`)
-        REFERENCES `queue-bot`.`Subjects` (`id_teacher`)
+    REFERENCES `queue-bot-kpi`.`Subjects` (`id_teacher`)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
